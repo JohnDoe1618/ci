370ceecbaabea1ec656bb07d86d156ad2c146c69
AveryOn <lnkonion2@gmail.com>
2024-06-26 17:19:38 +0300

    dev-projects

---
+++ src/services/testdata.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        id: 1,
+        name: 'first-project',
+        host: 'localhost',
+        port: 3001,
+        handshakeHash: '3fsw2-123as-23fas-1q2das',
+        createdAt: '2024-16-01 13:54:08.311 -0500',
+        updatedAt: '2024-16-01 13:54:08.311 -0500',
+    },
+    {
+        id: 2,
+        name: 'second-project',
+        host: 'localhost',
+        port: 3002,
+        handshakeHash: 'agdas-723as-23fas-1q2das',
+        createdAt: '2024-26-01 13:54:08.311 -0500',
+        updatedAt: '2024-26-01 13:54:08.311 -0500',
+    },
+    {
+        id: 3,
+        name: 'third-project',
+        host: 'localhost',
+        port: 3003,
+        handshakeHash: 'askas-190as-23fas-1q2das',
+        createdAt: '2024-12-01 13:54:08.311 -0500',
+        updatedAt: '2024-12-01 13:54:08.311 -0500',
+    },
+    {
+        id: 4,
+        name: 'fourth-project',
+        host: 'localhost',
+        port: 3004,
+        handshakeHash: 'asdas-asfa21-23fas-24r44af',
+        createdAt: '2024-02-01 13:54:08.311 -0500',
+        updatedAt: '2024-02-01 13:54:08.311 -0500',
+    },{
+        id: 5,
+        name: 'fifth-project',
+        host: 'localhost',
+        port: 3005,
+        handshakeHash: 'ghf34-123as-sdfgx342-1q2das',
+        createdAt: '2024-09-01 13:54:08.311 -0500',
+        updatedAt: '2024-09-01 13:54:08.311 -0500',
+    }
+]
